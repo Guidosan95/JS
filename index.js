c3ed0558e0e0a48aa5b010014679fb6018b9c9f5
Guidosan95 <guidosan95@outlook.com>
2022-04-03 20:20:00 -0300

    commit 1

---
+++ index.js
@@ -0,0 +1,176 @@
+const bienvenida = alert("Ahora si! Contame...");
+
+let inter= prompt("Esta/ Estan interesado/s en comprar Helado?");
+respue = "si";
+respue1= "no";
+
+if (inter == "si"){
+        alert("Genial! Voy a intentar de ayudarte en todo lo posible para que puedas realizar tu pedido con exactitud campeon/a")
+
+}
+else {
+        alert("Lamento que no sea lo que buscas! De igual manera te invito a que mires nuestro productos sin compromiso")
+
+}
+
+
+
+// if (inter == "si") {
+//     dineroGuido = parseInt (prompt("Cuanto dinero tiene Guido"));
+//     dineroJuanma = parseInt(prompt("Cuanto dinero tiene Juanma "));
+//     dineroGon = parseInt(prompt("Cuanto dinero tiene Gon"));
+//     dineroJuani = parseInt(prompt("Cuanto dinero tiene Juani"));
+// }
+// else {
+//     alert("Gracias por la honestidad");
+// }
+
+
+// if (dineroGuido >= 100 && dineroGuido < 120){
+//         alert("Te alcanza para una Vasito grande")
+//         alert ("Y te sobran " + (dineroGuido - 100))
+
+// }
+// else if (dineroGuido >= 120 && dineroGuido < 150){
+//         alert("Te alcanza para 1/4 KG")
+//         alert ("Y te sobran " + (dineroGuido - 120))
+
+// }
+// else if (dineroGuido >= 150 && dineroGuido < 180){
+//         alert("Te alcanza para un 1/2 KG")
+//         alert ("Y te sobran " + (dineroGuido - 150))
+
+// }
+// else if (dineroGuido >= 180 && dineroGuido < 200){
+//         alert("Te alcanza para 1 KG")
+//         alert ("Y te sobran " + (dineroGuido - 180))
+
+// }
+// else if (dineroGuido >= 200){
+//         alert("Te alcanza para una Heladeria como esta")
+//         alert ("Y te sobran " + (dineroGuido - 200))
+
+// }
+// else {
+//         alert("No te alcanza ni para un Vasito chiquito")
+
+// }
+
+// // Juanma
+
+// if (dineroJuanma >= 100 && dineroJuanma < 120){
+//         alert("Te alcanza para una Vasito grande")
+//         alert ("Y te sobran " + (dineroJuanma - 100))
+
+// }
+// else if (dineroJuanma >= 120 && dineroJuanma < 150){
+//         alert("Te alcanza para 1/4 KG")
+//         alert ("Y te sobran " + (dineroJuanma - 120))
+
+// }
+// else if (dineroJuanma >= 150 && dineroJuanma < 180){
+//         alert("Te alcanza para un 1/2 KG")
+//         alert ("Y te sobran " + (dineroJuanma - 150))
+
+// }
+// else if (dineroJuanma >= 180 && dineroJuanma < 200){
+//         alert("Te alcanza para 1 KG")
+//         alert ("Y te sobran " + (dineroJuanma - 180))
+
+// }
+// else if (dineroJuanma >= 200){
+//         alert("Te alcanza para una Heladeria como esta")
+//         alert ("Y te sobran " + (dineroJuanma - 200))
+
+// }
+// else {
+//         alert("No te alcanza ni para un Vasito chiquito")
+
+// }
+
+// // Gon 
+
+
+// if (dineroGon >= 100 && dineroGon < 120){
+//         alert("Te alcanza para una Vasito grande")
+//         alert ("Y te sobran " + (dineroGon - 100))
+
+// }
+// else if (dineroGon >= 120 && dineroGon < 150){
+//         alert("Te alcanza para 1/4 KG")
+//         alert ("Y te sobran " + (dineroGon - 120))
+
+// }
+// else if (dineroGon >= 150 && dineroGon < 180){
+//         alert("Te alcanza para un 1/2 KG")
+//         alert ("Y te sobran " + (dineroGon - 150))
+
+// }
+// else if (dineroGon >= 180 && dineroGon < 200){
+//         alert("Te alcanza para 1 KG")
+//         alert ("Y te sobran " + (dineroGon - 180))
+
+// }
+// else if (dineroGon >= 200){
+//         alert("Te alcanza para una Heladeria como esta")
+//         alert ("Y te sobran " + (dineroGon - 200))
+
+// }
+// else {
+//         alert("No te alcanza ni para un Vasito chiquito")
+
+// }
+
+// // Juani
+
+// if (dineroJuani >= 100 && dineroJuani < 120){
+//         alert("Te alcanza para una Vasito grande")
+//         alert ("Y te sobran " + (dineroJuani - 100))
+
+// }
+// else if (dineroJuani >= 120 && dineroJuani < 150){
+//         alert("Te alcanza para 1/4 KG")
+//         alert ("Y te sobran " + (dineroJuani - 120))
+
+// }
+// else if (dineroJuani >= 150 && dineroJuani < 180){
+//         alert("Te alcanza para un 1/2 KG")
+//         alert ("Y te sobran " + (dineroJuani - 150))
+
+// }
+// else if (dineroJuani >= 180 && dineroJuani < 200){
+//         alert("Te alcanza para 1 KG")
+//         alert ("Y te sobran " + (dineroJuani - 180))
+
+// }
+// else if (dineroJuani >= 200){
+//         alert("Te alcanza para una Heladeria como esta")
+//         alert ("Y te sobran " + (dineroJuani - 200))
+
+// }
+// else {
+//         alert("No te alcanza ni para un Vasito chiquito")
+
+// }
+
+// const tur = alert("Genial! Luego de conocer tu presupuesto, se te brindara un numero de turno para realizar tu pedido")
+
+// const turnos= 4;
+
+// for (let i = 1 ; i <= turnos; i++){
+//     let nombre = prompt('Ingresar nombre:');
+//     alert('Turno N° ' + i + ' para ' + nombre)
+// }
+
+// const genia= alert("Buenisimo! Ya tenemos todos los datos necesarios para poder continuar con tu compra si asi lo deseas.")
+// const fin = alert("Pregunto... Asi lo deseas ? Hasta que no digas la palabra magica, este bucle no se cerrara.")
+// let brom= prompt("Deseeas todavia seguir aqui?")
+
+// while (brom != "no") {
+//     alert('Tu respuesta es ' + brom + '. Veo que todavia no es momento de salir, no? O es que no recordas la palabra magica ?');
+
+//     brom= prompt("Deseeas todavia seguir aqui?");  
+
+// }
+
+// alert("Por fin haz salido de este bucle sin fin. Te felicito. Disfruta de tu compra querido")
---
+++ intro.js
@@ -0,0 +1,10 @@
+const bienvenida = alert("Hola. Bienvenido a Piquiwini");
+const logo = alert("Pulsa el Logo para ingresar a la pagina principal");
+
+function saludar(){
+
+        alert("hola buenas, como va?")
+        return "la funcion se ejecuto"
+}
+
+saludar()
