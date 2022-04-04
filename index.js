ec8a6851692413fb37096bee9714f469a99475e3
Guidosan95 <guidosan95@outlook.com>
2022-04-03 21:08:42 -0300

    commit3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const bienvenida = alert("Hola. Bienvenido a Piquiwini");
-const logo = alert("Pulsa el Logo para ingresar a la pagina principal");
+// const bienvenida = alert("Hola. Bienvenido a Piquiwini");
+// const logo = alert("Pulsa el Logo para ingresar a la pagina principal");
 
-function saludar(){
+// function saludar(){
 
-        alert("hola buenas, como va?")
-        return "la funcion se ejecuto"
-}
+//         alert("hola buenas, como va?")
+//         return "la funcion se ejecuto"
+// }
 
-saludar()
+// saludar()
